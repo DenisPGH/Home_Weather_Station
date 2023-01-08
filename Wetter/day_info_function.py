d319dc66ae4b184913d59e7bbe43f7f48d0310ef
Denislav <deni_9000_1990@abv.bg>
2023-01-08 12:23:20 +0100

    created function for returning right info for today if is

--- Wetter/day_info_function.py
+++ Wetter/day_info_function.py
@@ -1,8 +1,7 @@
-import json
-
+import csv
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup as soup
-import re
 from database_function import TIME_DATE
 
 
@@ -54,52 +53,54 @@ class Ortodox:
         self.holi = 'holi'
         self.date_day_info = {}  # {date: [{day:'P', holi:'absved'} , {}] , }
         self.counter = 0
-
-
-    def store_holidays_into_json(self):
-        """
-        write the holidays into json file
-        :return:
-        """
-
-        dict_store=self.create_dictionary_holidays()
-        with open('holdays_2023.json','w') as file:
-            file.write(json.dumps(dict_store))
+        self.LINK_CSV_HOLIDAYS= 'holdays_2023.csv'
+        self.colums=['month','date','inforamtion']
 
 
     def current_day_ortodox(self):
-        pass
-
-
-    def create_dictionary_holidays(self):
         """
-        :return: dictionary with info {1: [ {day: 1, 'holi': "<String>"}, {} ]  }
+        :return: string with the information for current day
+        """
+        month=int(self.date.DATE().split("-")[1])
+        day = int(self.date.DATE().split("-")[2])
+
+        data= pd.read_csv(self.LINK_CSV_HOLIDAYS,sep='|',names=self.colums,header=None)
+        result=data.loc[(data['month'] == month) & (data['date'] == day)]['inforamtion'].values.tolist()
+        if result:
+            return result[0]
+        else:
+            return ''
+
+    def create_csv_file__holidays(self):
+        """
+        create csv file with the month| day | information
+        :return:
         """
         try:
             response = requests.post('https://bgcal.com/%D0%BF%D1%80%D0%B0%D0%B7%D0%BD%D0%B8%D1%86%D0%B8/2023', params=self.params, headers=self.headers)
             html = soup(response.text, 'html.parser')
-            for a in range(5, 19):
-                if a not in self.werbung:
-                    self.counter+=1
-                    info = html.select(f'body > div.container > div:nth-child({a})> div > div > div.list-group > div')
-                    self.date_day_info[self.counter] = []
-                    for b in info:
-                        holiday = b.select('h4')[0].text.split(':')[1].strip().split('-')[0]
-                        date = b.select('h4')[0].text.split(':')[0].split(" ")[0].strip()
-                        current_day_fest = {}
-                        current_day_fest[self.day] = date
-                        current_day_fest[self.holi] = holiday
-                        self.date_day_info[self.counter].append(current_day_fest)
-            return self.date_day_info
+            with open(f'{self.LINK_CSV_HOLIDAYS}', mode='w', encoding='utf-8', newline='') as csv_write:
+                for a in range(5, 19):
+                    if a not in self.werbung:
+                        self.counter+=1
+                        info = html.select(f'body > div.container > div:nth-child({a})> div > div > div.list-group > div')
+                        self.date_day_info[self.counter] = []
+                        for b in info:
+                            holiday = b.select('h4')[0].text.split(':')[1].strip().split('-')[0]
+                            date = b.select('h4')[0].text.split(':')[0].split(" ")[0].strip()
+                            file = csv.writer(csv_write, delimiter='|', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                            file.writerow([self.counter, date, holiday])
+
 
 
         except:
-            return self.date_day_info
+            pass
 
 
 
 
-if __name__ == "__main__":
-
-    k=Ortodox()
-    print(k.store_holidays_into_json())
+# if __name__ == "__main__":
+#
+#     k=Ortodox()
+#     #print(k.create_csv_file__holidays())
+#     print(k.current_day_ortodox())
