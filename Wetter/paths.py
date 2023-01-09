80b370284cc20614f696bd4c8d23929371723943
Nanoo Botev <nanoobotev@abv.bg>
2023-01-09 21:00:41 +0100

    changed values

--- Wetter/paths.py
+++ Wetter/paths.py
@@ -8,3 +8,5 @@ import os
 
 PATH_HOLIDAYS= 'holdays_2023.csv' if os.getlogin()=='Owner' else '/home/raspi/Desktop/watch/Home_Weather_Station/Wetter/holdays_2023.csv'
 PATH_DB ='wetter_DB_1.csv' if os.getlogin()=='Owner' else '/home/raspi/Desktop/watch/Home_Weather_Station/Wetter/wetter_DB.csv'
+
+print(os.getlogin())
