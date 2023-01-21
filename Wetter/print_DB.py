b2d8a70ab083b7a7149533f0cc82a6e801ed2567
Denislav <deni_9000_1990@abv.bg>
2023-01-21 09:37:08 +0100

    removed C sign from temp outside

--- Wetter/print_DB.py
+++ Wetter/print_DB.py
@@ -9,18 +9,18 @@ if USER ==USER_CLIENT:
     # 1.inital db
 
     # # 2.create table
-    # db.create_table()
+    db.create_table()
     # # 3.copy csv
     # #db.csv__to_sqlite()
     #
     # # 4. create table outside
-    # db.create_table_outside()
+    db.create_table_outside()
     # 4.1. add column to outside
-    # db.add_column_to_table_outside()
+    db.add_column_to_table_outside()
     # 4.2. add column pressure to outside
-    # db.add_column_pressure__to_table_outside()
+    db.add_column_pressure__to_table_outside()
     # # 5. create table cpu
-    # db.create_table_cpu()
+    db.create_table_cpu()
 
     #db.clear_table(db.NAME_TABLE_OUTSIDE)
 
